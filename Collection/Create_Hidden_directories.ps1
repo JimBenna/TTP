f1c4fd7cf089a175076c396dc4f133f8f42bd0d8
Jim Benna <jim.benna@gmail.com>
2025-06-20 19:37:58 +0200

    Added Technic ID

--- Collection/Create_Hidden_directories.ps1
+++ Collection/Create_Hidden_directories.ps1
@@ -1,7 +1,8 @@
 # Create Hidden directories
+# Technic ID : T1134.001
 Clear-Host
 Write-Output "==============================================================================="
-Write-Output "Create Hidden directories"
+Write-Output "Create Hidden directories (T1134.001)"
 Write-Output "==============================================================================="
 New-Item -ItemType directory -Path "$env:PUBLIC\exf";
 $hidden_dir=Get-Item "$env:PUBLIC\exf" -Force;
