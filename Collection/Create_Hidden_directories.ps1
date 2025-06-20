74119fd8bf61c681cd63de7f37fc853203ef4c44
Jim Benna <jim.benna@gmail.com>
2025-06-20 19:35:20 +0200

    Create HIdden directories and collect public ip addresses

---
+++ Collection/Create_Hidden_directories.ps1
@@ -0,0 +1,11 @@
+# Create Hidden directories
+Clear-Host
+Write-Output "==============================================================================="
+Write-Output "Create Hidden directories"
+Write-Output "==============================================================================="
+New-Item -ItemType directory -Path "$env:PUBLIC\exf";
+$hidden_dir=Get-Item "$env:PUBLIC\exf" -Force;
+$hidden_dir.attributes="Hidden";
+New-Item -ItemType directory -Path "$env:PUBLIC\Toolz";
+$hidden_Toolz_dir=Get-Item "$env:PUBLIC\Toolz" -Force;
+$hidden_Toolz_dir.attributes="Hidden";
