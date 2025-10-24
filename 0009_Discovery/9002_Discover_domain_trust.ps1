e35963c83b0611604fd848939941ee0d88a08547
Jim Benna <jim.benna@gmail.com>
2025-10-24 11:16:17 +0200

    Change names

---
+++ 0009_Discovery/9002_Discover_domain_trust.ps1
@@ -0,0 +1,8 @@
+# Domain Trust Discovery
+# Technic ID : T1482
+Clear-Host
+Write-Output "==============================================================================="
+Write-Output "Domain Trust Discovery with encoded command (T1482)"
+Write-Output "==============================================================================="
+#
+Start-Process -Filepath "powershell.exe" -ArgumentList "-EncodedCommand bgBsAHQAZQBzAHQAIAAvAGQAbwBtAGEAaQBuAF8AdAByAHUAcwB0AHMA"
