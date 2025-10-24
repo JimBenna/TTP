6954cfc903d53408af6c2ee8ba165d4e1d6e8574
Jim Benna <jim.benna@gmail.com>
2025-10-24 11:39:05 +0200

    discover domain controllers

---
+++ 0009_Discovery/9002_Discover_Domain_Controllers.ps1
@@ -0,0 +1,9 @@
+# Domain Trust Discovery
+# Technic ID : T1018
+Clear-Host
+Write-Output "==============================================================================="
+Write-Output "Remote system discovery (T1018)"
+Write-Output "Identify the remote domain controllers and save the information to a file"
+Write-Output "==============================================================================="
+#
+Start-Process -Filepath "cmd.exe" -ArgumentList "/c nltest /dsgetdc:$env:USERDOMAIN >$env:PUBLIC\exf\Domain_controllers.txt" -NoNewWindow -Wait
