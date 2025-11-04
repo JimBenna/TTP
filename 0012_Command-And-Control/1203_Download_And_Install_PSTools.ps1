a53404ca8c732d26fa7d916cc9b363a8c26d0cff
Jim Benna <jim.benna@gmail.com>
2025-11-04 12:02:51 +0100

    Updated the PSTools download to catch errors

--- 0012_Command-And-Control/1203_Download_And_Install_PSTools.ps1
+++ 0012_Command-And-Control/1203_Download_And_Install_PSTools.ps1
@@ -4,23 +4,33 @@ Clear-Host
 Write-Output "==============================================================================="
 Write-Output "Download and installs PSTools (T1105)"
 Write-Output "==============================================================================="
-#
-$ArchiveFile = "PSTools.zip"
+#$ArchiveFile = "PSTools.zip"
 $DestinationDirectory = "$env:PUBLIC\PSTools"
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True };
-$web = (New-Object System.Net.WebClient);
-$result = $web.DownloadFile("https://download.sysinternals.com/files/PSTools.zip", "$ArchiveFile");
-New-Item -ItemType "directory" $DestinationDirectory -Force;
-$psdir=Get-Item "$DestinationDirectory" -Force;
-$psdir.attributes="hidden";
+$Full_Destination = "$env:PUBLIC\Toolz\$ArchiveFile"
+$LogFile = "$env:PUBLIC\exf\pstools_down_extract.txt"; 
+$DownloadURL = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+$CompleteCommand = "Invoke-WebRequest -Uri" + " $DownloadURL" + " -outfile " + "$Full_Destination";
+try {
+    Invoke-Expression  "$CompleteCommand";
+    Write-Output "Successfully downloaded [$ArchiveFile] to $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+    New-Item -ItemType "directory" $DestinationDirectory -Force;
+    $psdir = Get-Item "$DestinationDirectory" -Force;
+    $psdir.attributes = "hidden";
 
-if ([System.IO.File]::Exists("$ArchiveFile")) {
-    Add-Type -Assembly 'System.IO.Compression.FileSystem';
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$ArchiveFile", "$DestinationDirectory");
-    exit 0;
-} 
-else {
-    Write-Output "The file ["$ArchiveFile"] does not exists !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
-    exit 1;
-};
+    if ([System.IO.File]::Exists("$ArchiveFile")) {
+        Add-Type -Assembly 'System.IO.Compression.FileSystem';
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$ArchiveFile", "$DestinationDirectory");
+        Write-Output "The file [$ArchiveFile] has been extracted to  $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
+        Get-ChildItem -Path $DestinationDirectory | Out-File -FilePath $LogFile -Encoding ascii -Append
+#        exit 0;
+    } 
+    else {
+        Write-Output "The file [$ArchiveFile] does not exists !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+#        exit 2;
+    };
 
+}
+catch {
+    Write-Output "The download of [$ArchiveFile] has failed : $($_.Exception.Message)" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+#    exit 1;   
+}
