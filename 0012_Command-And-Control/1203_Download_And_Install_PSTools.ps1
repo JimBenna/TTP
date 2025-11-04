25aa546c43fddf68ded9101b6778282a5dd4adc5
Jim Benna <jim.benna@gmail.com>
2025-11-04 12:03:28 +0100

    error codes management

--- 0012_Command-And-Control/1203_Download_And_Install_PSTools.ps1
+++ 0012_Command-And-Control/1203_Download_And_Install_PSTools.ps1
@@ -22,15 +22,15 @@ try {
         [System.IO.Compression.ZipFile]::ExtractToDirectory("$ArchiveFile", "$DestinationDirectory");
         Write-Output "The file [$ArchiveFile] has been extracted to  $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
         Get-ChildItem -Path $DestinationDirectory | Out-File -FilePath $LogFile -Encoding ascii -Append
-#        exit 0;
+        exit 0;
     } 
     else {
         Write-Output "The file [$ArchiveFile] does not exists !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
-#        exit 2;
+        exit 2;
     };
 
 }
 catch {
     Write-Output "The download of [$ArchiveFile] has failed : $($_.Exception.Message)" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
-#    exit 1;   
+    exit 1;   
 }
