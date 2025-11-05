1b74137b9961f81b5a002d6dc8ffce5410765790
Jim Benna <jim.benna@gmail.com>
2025-11-05 17:03:20 +0100

    Read each lines of files of restulting of subnet scan

--- 0009_Discovery/9013_Subnet_Information_And_Scan.ps1
+++ 0009_Discovery/9013_Subnet_Information_And_Scan.ps1
@@ -9,6 +9,11 @@ $LogFile = "$env:PUBLIC\exf\network_gather_scan.txt";
 $DestinationDirectory = "$env:PUBLIC\Toolz\fping_x64_4.2"
 $BinaryFile = "fping.exe"
 $FullPathBinary = "$DestinationDirectory" + "\" + "$BinaryFile"
+$PSToolsDirectory = "$env:PUBLIC\Pstools"
+$PSPingBinary = "psping64.exe"
+$ExfDirectory = "$env:PUBLIC\exf\"
+$FileNameMask = "Scan_with_if_"
+$FileNameExtension = ".txt"
 # Put connected Interfaces in an Array
 $ResultIpIf = Get-NetIPInterface | Where-Object { $_.ConnectionState -eq 'Connected' }
 #Write-Output "Result :" $ResultIpIf
@@ -53,51 +58,51 @@ foreach ($Node in $ResultNet) {
     }
 }
 
-$NetIpv4Array=Get-NetIPConfiguration |
-    Where-Object { $_.NetAdapter.Status -eq "Up" } |
-    ForEach-Object {
-        $interface = $_
-        # IPv4
-        $interface.IPv4Address | ForEach-Object {
-            $ip = $_.IPAddress
-            $prefix = $_.PrefixLength
-            $mask = [System.Net.IPAddress]::Parse(
-                (0..3 | ForEach-Object {
-                    if ($_ -lt $prefix / 8) { [byte]255 }
-                    elseif ($prefix -gt $_ * 8) { [byte]([Math]::Pow(2, 8 - ($prefix % 8)) - 1) }
-                    else { [byte]0 }
-                }) -join '.'
-            )
-            $ipBytes = [System.Net.IPAddress]::Parse($ip).GetAddressBytes()
-            $maskBytes = $mask.GetAddressBytes()
-            $networkBytes = for ($i = 0; $i -lt 4; $i++) {
-                [byte]($ipBytes[$i] -band $maskBytes[$i])
-            }
-            $network = [System.Net.IPAddress]::new($networkBytes).ToString()
-            [PSCustomObject]@{
-                IfId      = $interface.NetAdapter.InterfaceIndex
-                Interface = $interface.NetAdapter.InterfaceDescription
-                Type      = "IPv4"
-                IfAddress =  $ip
-                Prefix    = $prefix
-                NetMask   = $mask
-                Subnet    = "$network/$prefix"
-
-            }
+$NetIpv4Array = Get-NetIPConfiguration |
+Where-Object { $_.NetAdapter.Status -eq "Up" } |
+ForEach-Object {
+    $interface = $_
+    # IPv4
+    $interface.IPv4Address | ForEach-Object {
+        $ip = $_.IPAddress
+        $prefix = $_.PrefixLength
+        $mask = [System.Net.IPAddress]::Parse(
+            (0..3 | ForEach-Object {
+                if ($_ -lt $prefix / 8) { [byte]255 }
+                elseif ($prefix -gt $_ * 8) { [byte]([Math]::Pow(2, 8 - ($prefix % 8)) - 1) }
+                else { [byte]0 }
+            }) -join '.'
+        )
+        $ipBytes = [System.Net.IPAddress]::Parse($ip).GetAddressBytes()
+        $maskBytes = $mask.GetAddressBytes()
+        $networkBytes = for ($i = 0; $i -lt 4; $i++) {
+            [byte]($ipBytes[$i] -band $maskBytes[$i])
         }
-        # IPv6
-#        $interface.IPv6Address | Where-Object { $_.IsLinkLocal -eq $false } | ForEach-Object {
-#            [PSCustomObject]@{
-#                IfId      = $interface.NetAdapter.InterfaceIndex
-#                Interface = $interface.NetAdapter.InterfaceDescription
-#                Type      = "IPv6"
-#                IfAddress =  $interface.NetAdapter.IPv6Address
-#                Subnet    = "$($_.IPAddress)/$($_.PrefixLength)"
-#            }
-#        }
-    } 
-    $NetIpv4Array | Format-Table
-#    $NetIpv6Array | Format-Table
+        $network = [System.Net.IPAddress]::new($networkBytes).ToString()
+        [PSCustomObject]@{
+            IfId      = $interface.NetAdapter.InterfaceIndex
+            Interface = $interface.NetAdapter.InterfaceDescription
+            Type      = "IPv4"
+            IfAddress = $ip
+            Prefix    = $prefix
+            NetMask   = $mask
+            Subnet    = "$network/$prefix"
+
+        }
+    }
+    # IPv6
+    #        $interface.IPv6Address | Where-Object { $_.IsLinkLocal -eq $false } | ForEach-Object {
+    #            [PSCustomObject]@{
+    #                IfId      = $interface.NetAdapter.InterfaceIndex
+    #                Interface = $interface.NetAdapter.InterfaceDescription
+    #                Type      = "IPv6"
+    #                IfAddress =  $interface.NetAdapter.IPv6Address
+    #                Subnet    = "$($_.IPAddress)/$($_.PrefixLength)"
+    #            }
+    #        }
+} 
+# $NetIpv4Array | Format-Table
+# $NetIpv6Array | Format-Table
 
 
 #$ConnectedInterfaceArray|Format-Table
@@ -130,12 +135,46 @@ $SortedNetIpv4Array | Format-Table -AutoSize | Out-File -FilePath "$LogFile" -En
 if ([System.IO.File]::Exists("$FullPathBinary")) {
     Write-Output "The file [$BinaryFile] exists in $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
     $SortedNetIpv4Array | ForEach-Object {
-               $Log_Scan="$env:PUBLIC\exf\Scan_with_if_"+"$($_.ifAddress)"+".txt"
-               $CliParameters = "-4 -q -A -a -R -g " + "$($_.subnet)"
-               Start-Process -Filepath $FullPathBinary -ArgumentList $CliParameters -RedirectStandardOutput $Log_Scan -WindowStyle Hidden -Wait
-               Write-Output "The ouptut file [$Log_Scan] has been created in $DestinationDirectory" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
+        $Log_Scan = "$env:PUBLIC\exf\Scan_with_if_" + "$($_.ifAddress)" + ".txt"
+        $CliParameters = "-4 -q -A -a -R -g " + "$($_.subnet)"
+        Start-Process -Filepath $FullPathBinary -ArgumentList $CliParameters -RedirectStandardOutput $Log_Scan -WindowStyle Hidden -Wait
+        Write-Output "The ouptut file [$Log_Scan] has been created" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
     }
-    exit 0;
+    #    exit 0;
+
+    # Checks if PSPING exists
+    if ([System.IO.File]::Exists("$PSToolsDirectory\$PSPingBinary")) {
+        Write-Output "The file [$PSPingBinary] has been found in  $PSToolsDirectory !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+
+
+        # Avec compteurs et statistiques
+        $TotalFiles = 0
+        $TotalLines = 0
+        Get-ChildItem -Path "$ExfDirectory" -Filter "$FileNameMask*$FileNameExtension" | ForEach-Object {
+            $TotalFiles++
+            Write-Output "`Analyze file named : $($_.Name)" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+    
+            $FileLinesNumber = Get-Content $_.FullName
+            $TotalLines += $FileLinesNumber.Count
+    
+            $FileLinesNumber | ForEach-Object -Begin { $LineNumber = 1 } -Process {
+                # Traitement avec numérotation
+                Write-Output " Aanlyzing line [$LineNumber] : Scanning  $_" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
+                $LineNumber++
+            }
+    
+            Write-Output "Total of lines computed : $($FileLinesNumber.Count)" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
+        }
+
+        Write-Output "`Process ended : $TotalFiles files computed, and $TotalLines lines" | Out-File -FilePath "$LogFile" -Encoding ascii -Append;
+
+    }
+    else {
+        Write-Output "The file [$PSPingBinary] has not been found in  $PSToolsDirectory !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
+        exit 2;   
+    }
+
+
 } 
 else {
     Write-Output "The file [$BinaryFile] can not be found in  $DestinationDirectory !!!" | Out-File -FilePath "$LogFile" -Encoding ascii -Append; 
