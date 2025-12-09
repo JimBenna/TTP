c9907b634d37cb34df0372479dc6e006cf9a43bb
Jim Benna <jim.benna@gmail.com>
2025-12-09 16:06:50 +0100

    Updated launching script

--- 0000_1st_steps/Global_scripts_run.ps1
+++ 0000_1st_steps/Global_scripts_run.ps1
@@ -1,16 +1,23 @@
 # Launch All scripts
-
 Clear-Host
+Write-output "`n"
+Write-output "`n"
 Write-Output "======================================================================================"
 Write-Output "This scripts read a file and launch every scripts with a time wait between each script"
 Write-Output "======================================================================================"
 #
-$InputFile="scripts_list.txt"
+$InputFile= $PSScriptRoot+"\scripts_list.txt"
+$parentDirectory = Split-Path -Path $PSScriptRoot -Parent
 $CommandsArray = Get-Content -Path $InputFile
+$PauseTimerInSeconds = "4"
 
 # Loops all commands that have been stored in the array
 foreach ($Command in $CommandsArray)
 {
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-Noprofile -File $Command" -WindowStyle Hidden
-    Start-Sleep -Seconds 10
+    Write-Output "Runing script : $Command"
+    write-output "Parent Directory : $parentDirectory"
+#    Start-Process -FilePath "powershell.exe" -ArgumentList "-Noprofile -File $Command" -WindowStyle Hidden -Wait
+    Write-Output "Waiting $PauseTimerInSeconds before launching anhother script ..."
+    Write-output "`n"
+    Start-Sleep -Seconds $PauseTimerInSeconds
 }
