e9c0b36ae82088862cb4876592e838e95329f84a
Jim Benna <jim.benna@gmail.com>
2026-01-07 16:18:48 +0100

    error output code in scheduled task script corrected

--- 0005_Persistence/5001_Add_Scheduled_task.ps1
+++ 0005_Persistence/5001_Add_Scheduled_task.ps1
@@ -16,15 +16,14 @@ if ([System.IO.File]::Exists("$PsShutdown")) {
             $ServiceCmd = "C:\Windows\System32\schtasks.exe /Create /SC DAILY /TN '$TaskName' /ST 21:45 /TR '$RunTask'";
             Write-Output "Launch command : " $ServiceCmd  | Out-File -FilePath "$PsShutLogFile" -Append -Encoding ascii;
             Invoke-Expression "$ServiceCmd"  | Out-File -FilePath "$PsShutLogFile" -Append -Encoding ascii;
-
-    }
+            exit 0;
+        }
     catch {
         Write-Output "Error while excecuting command $ServiceCmd error code : $($_.Exception.Message)" | Out-File -FilePath "$PsShutLogFile" -Encoding ascii -Append;
         exit 2;      
-    }                        
-    exit 0;
-}
+        }                        
+    }
 else {
     Write-Output "The command $PsShutdown does not exists !!!" | Out-File -FilePath "$PsShutLogFile" -Encoding ascii -Append;
     exit 1;
-}
+    }
